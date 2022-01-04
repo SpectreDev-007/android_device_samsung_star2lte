8f8722536ab4b91bb311f60f60e0e1f87d38198e
CyderXXV <87647653+CyderVII28x03@users.noreply.github.com>
2022-01-04 16:35:34 +0700

    I forgot lmfao.

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -50,6 +50,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.036 7805805 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.PDA=N960FXXU6FTK1 \
+    ro.build.PDA=G965FXXUCFTK1 \
     ro.build.fingerprint=google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys
 
